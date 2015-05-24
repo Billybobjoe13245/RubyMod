ffbfd8c79aebca5792dbc4eddf31f141d9a83399
Ricardo C. Kifuri <ricky.rocks104@gmail.com>
2015-05-24 13:46:11 -0600

    RubyMod

--- x10/mx/billybobjoe13245/rubymod/Reference.java
+++
@@ -1,9 +0,0 @@
-package x10.mx.billybobjoe13245.rubymod;
-
-public class Reference {
-	public static final String MOD_ID = "rubymod";
-	public static final String MOD_NAME = "Ruby Mod";
-	public static final String VERSION = "4.0.0 Alpha";
-	public static final String CLIENT_PROXY_CLASS = "x10.mx.billybobjoe13245.rubymod.proxy.clientProxy";
-	public static final String SERVER_PROXY_CLASS = "x10.mx.billybobjoe13245.rubymod.proxy.commonProxy";
-}
